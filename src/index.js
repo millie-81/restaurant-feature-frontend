9ff240e322d74a7ec97dc0a0adf3fc1b13345294
beike18 <Ottawafang8!1>
2021-09-06 18:21:51 -0400

    add files

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {App} from "./App";
-
+// feature part
 ReactDOM.render(
     <React.StrictMode>
         <App/>
+
     </React.StrictMode>,
     document.getElementById('root')
 );
